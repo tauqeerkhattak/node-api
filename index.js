a2199ab7098190a1b1810929fde43d0d9d7375fb
tauqeerkhattak <17sw72@students.muet.edu.pk>
2023-03-29 11:03:20 +0500

    node template added

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/products', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "products api route",
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
